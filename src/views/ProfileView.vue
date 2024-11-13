9c1939755bfb708a59ae949e9a307409dd57fec3
maxestwell <maxwellpcorcoran@gmail.com>
2024-11-13 11:46:28 +1100

    waho

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -27,13 +27,31 @@ onMounted(() => {
     <h3>stickers</h3>
     <img src="/src/assets/images/sticker.png" alt="sticker" />
   </div>
+  <div class="profileimg"></div>
 </template>
 
 <style scoped>
+.profileimg {
+  background-image: url('/src/assets/images/cloudmtns.jpg');
+  background-size: cover;
+  background-position: center;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1; /* Send the background image to the back */
+}
+
 .title-container {
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
+}
+
+.about {
+  position: relative; /* Ensure the about section is positioned above the background image */
+  z-index: 1; /* Ensure the about section is above the background image */
 }
 
 h2 {
