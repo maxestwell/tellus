b11eaa41e25a7163953acb6f1ef1d490f447fcf3
maxestwell <maxwellpcorcoran@gmail.com>
2024-11-26 13:53:33 +1100

    uhh ya

--- src/views/ConfirmationView.vue
+++ src/views/ConfirmationView.vue
@@ -27,7 +27,7 @@ const selectAnswer = (answer) => {
         <h1>Thank You!</h1>
         <p>Your response has been submitted to the “Would You Rather?” Archive.</p>
         <p>ALSO, here's a sticker to add to your profile!</p>
-        <img src="/tellus/images/sticker.png" alt="Sticker" />
+        <!-- <img src="/tellus/images/sticker.png" alt="Sticker" /> -->
         <div class="button-choices">
           <button @click="$router.push('/')">Return Home</button>
         </div>
